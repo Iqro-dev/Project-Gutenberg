48a9e34307cb9aef919aa1556da43b9c994b1e44
Iqro-riv <arumisokolowska@gmail.com>
2022-05-19 20:01:23 +0200

    feat(books): animation while loading

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -69,11 +69,23 @@ export default function Books() {
               </div>
             ))
           ) : (
-            <div>
-              <svg
-                className="bg-black animate-spin h-5 w-5 mr-3 ..."
-                viewBox="0 0 24 24"
-              ></svg>
+            <div className="flex flex-row gap-4">
+              <svg className="animate-spin h-5 w-5" viewBox="0 0 24 24">
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              Loading...
             </div>
           )}
         </div>
