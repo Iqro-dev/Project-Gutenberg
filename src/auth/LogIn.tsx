903476527e9e5813dbc33ef0f2aff27e6e7e7bc2
Iqro-riv <arumisokolowska@gmail.com>
2022-05-29 16:08:09 +0200

    feat(auth): auth finished

--- src/auth/LogIn.tsx
+++ src/auth/LogIn.tsx
@@ -1,7 +1,12 @@
 import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { faG } from "@fortawesome/free-solid-svg-icons";
@@ -10,6 +15,8 @@ export default function LogIn() {
   const auth = getAuth();
   const navigate = useNavigate();
   const [authing, setAuthing] = useState(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const signInWithGoogle = async () => {
     setAuthing(true);
@@ -26,38 +33,54 @@ export default function LogIn() {
       });
   };
 
-  interface Input {
-    label: string;
-    inputType: string;
-  }
+  const signIn = () => {
+    setAuthing(true);
 
-  const inputs: Input[] = [
-    {
-      label: "Email",
-      inputType: "email",
-    },
-    {
-      label: "Password",
-      inputType: "password",
-    },
-  ];
+    signInWithEmailAndPassword(auth, email, password)
+      .then((response) => {
+        console.log(response.user.uid);
+        navigate("/");
+        localStorage.setItem("authorized", "true");
+      })
+      .catch((error) => {
+        console.log(error);
+        setAuthing(false);
+      });
+  };
 
   return (
     <div className="flex flex-col justify-center items-center w-full h-full pt-12 gap-6 pb-96">
       <span className="text-4xl font-['Poppins'] font-normal">Log In</span>
 
       <form className="flex flex-col xl:w-1/5 p-4 gap-8">
-        {inputs.map(({ label, inputType }, index) => (
-          <div key={index} className="flex flex-col gap-2">
-            <label className="text-sm font-['Poppins'] font-normal">
-              {label}
-            </label>
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-['Poppins'] font-normal">Email</label>
 
-            <input type={inputType} className="outline rounded-md p-2" />
-          </div>
-        ))}
+          <input
+            type="email"
+            className="outline rounded-md p-2"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-        <button type="submit">
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-['Poppins'] font-normal">
+            Password
+          </label>
+
+          <input
+            type="password"
+            className="outline rounded-md p-2"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            signIn();
+          }}
+        >
           <FontAwesomeIcon icon={faArrowAltCircleRight} size="2x" />
         </button>
 
--- src/auth/SignUp.tsx
+++ src/auth/SignUp.tsx
@@ -1,13 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleRight, faG } from "@fortawesome/free-solid-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { useState } from "react";
 
 export default function SignUp() {
   const auth = getAuth();
   const navigate = useNavigate();
   const [authing, setAuthing] = useState(false);
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
 
   const signInWithGoogle = async () => {
     setAuthing(true);
@@ -16,6 +25,7 @@ export default function SignUp() {
       .then((response) => {
         console.log(response.user.uid);
         navigate("/");
+        localStorage.setItem("authorized", "true");
       })
       .catch((error) => {
         console.log(error);
@@ -23,41 +33,67 @@ export default function SignUp() {
       });
   };
 
-  interface Input {
-    label: string;
-    inputType: string;
-  }
+  const register = () => {
+    setAuthing(true);
 
-  const inputs: Input[] = [
-    {
-      label: "Email",
-      inputType: "email",
-    },
-    {
-      label: "Password",
-      inputType: "password",
-    },
-    {
-      label: "Password Confirm",
-      inputType: "password",
-    },
-  ];
+    if (password === confirmPassword)
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((response) => {
+          console.log(response.user.uid);
+          navigate("/");
+          localStorage.setItem("authorized", "true");
+        })
+        .catch((error) => {
+          console.log(error);
+          setAuthing(false);
+        });
+  };
+
   return (
     <div className="flex flex-col justify-center items-center w-full h-full pt-12 gap-6 pb-96">
       <span className="text-4xl font-['Poppins'] font-normal">Sign Up</span>
 
       <form className="flex flex-col xl:w-1/5 p-4 gap-8">
-        {inputs.map(({ label, inputType }, index) => (
-          <div key={index} className="flex flex-col gap-2">
-            <label className="text-sm font-['Poppins'] font-normal">
-              {label}
-            </label>
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-['Poppins'] font-normal">Email</label>
 
-            <input type={inputType} className="outline rounded-md p-2" />
-          </div>
-        ))}
+          <input
+            type="email"
+            className="outline rounded-md p-2"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-        <button type="submit">
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-['Poppins'] font-normal">
+            Password
+          </label>
+
+          <input
+            type="password"
+            className="outline rounded-md p-2"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-['Poppins'] font-normal">
+            Confirm Password
+          </label>
+
+          <input
+            type="password"
+            className="outline rounded-md p-2"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            register();
+          }}
+        >
           <FontAwesomeIcon icon={faArrowAltCircleRight} size="2x" />
         </button>
 
